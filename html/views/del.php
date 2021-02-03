703dac5294198e4b7b4499bc61eb838cf3e3930d
Sultan Baharuddin <50097679+Roooodie@users.noreply.github.com>
2021-02-03 13:52:27 +0800

    change language

--- html/views/del.php
+++ html/views/del.php
@@ -1,41 +1,41 @@
 <h3 class="uk-heading-line">
-    <span>删除学生信息</span>
+    <span>Delete Student Information</span>
 </h3>
 <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
     <form class="uk-form-stacked" action="/delete?id=<?php __($id ?? ''); ?>" method="post">
         <div class="uk-alert-danger" uk-alert>
-            <p>数据无价，请再次确认是否删除！</p>
+            <p>Data is priceless, please confirm whether to delete again!</p>
         </div>
 
         <input name="id" type="hidden" value="<?php __($id ?? ''); ?>">
 
         <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">学号</label>
+            <label class="uk-form-label" for="form-stacked-text">Student Number</label>
             <div class="uk-form-controls">
                 <?php __($no ?? ''); ?>
             </div>
         </div>
 
         <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">姓名</label>
+            <label class="uk-form-label" for="form-stacked-text">Name</label>
             <div class="uk-form-controls">
                 <?php __($name ?? ''); ?>
             </div>
         </div>
 
         <div class="uk-margin">
-            <div class="uk-form-label">性别</div>
+            <div class="uk-form-label">Gender</div>
             <div class="uk-form-controls">
-                <?php echo(($gender ?? '0') === 0 ? '男' : '女') ?>
+                <?php echo(($gender ?? '0') === 0 ? 'Male' : 'Female') ?>
             </div>
         </div>
 
         <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">电话号码</label>
+            <label class="uk-form-label" for="form-stacked-text">Phone Number</label>
             <div class="uk-form-controls">
                 <?php __($phone ?? ''); ?>
             </div>
         </div>
-        <button type="submit" class="uk-button uk-button-danger">确认删除</button>
+        <button type="submit" class="uk-button uk-button-danger">Apply</button>
     </form>
 </div>

762d4fabbf31ef2752c3b4cc24f99917e108afc1
Sultan Baharuddin <50097679+Roooodie@users.noreply.github.com>
2021-02-03 13:49:50 +0800

    Change Language

--- html/views/edit.php
+++ html/views/edit.php
@@ -1,5 +1,5 @@
 <h3 class="uk-heading-line">
-    <span>修改学生信息</span>
+    <span>Modify Student Information</span>
 </h3>
 <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
     <form class="uk-form-stacked" action="/edit?id=<?php __($id ?? ''); ?>" method="post">
@@ -16,34 +16,34 @@
         <div class="uk-margin">
             <label class="uk-form-label" for="form-stacked-text">学号</label>
             <div class="uk-form-controls">
-                <input class="uk-input" name="no" type="text" placeholder="请输入数字学号" value="<?php __($no ?? ''); ?>">
+                <input class="uk-input" name="no" type="text" placeholder="Input Student Number" value="<?php __($no ?? ''); ?>">
             </div>
         </div>
 
         <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">姓名</label>
+            <label class="uk-form-label" for="form-stacked-text">Name</label>
             <div class="uk-form-controls">
-                <input class="uk-input" name="name" type="text" placeholder="请输入学生姓名" value="<?php __($name ?? ''); ?>">
+                <input class="uk-input" name="name" type="text" placeholder="Input Student Name" value="<?php __($name ?? ''); ?>">
             </div>
         </div>
 
         <div class="uk-margin">
-            <div class="uk-form-label">性别</div>
+            <div class="uk-form-label">Gender</div>
             <div class="uk-form-controls">
                 <label><input class="uk-radio" type="radio" name="gender"
-                              value="0" <?php echo(($gender ?? '0') === 0 ? 'checked' : '') ?>>男</label>
+                              value="0" <?php echo(($gender ?? '0') === 0 ? 'checked' : '') ?>>Male</label>
                 <label><input class="uk-radio" type="radio" name="gender"
-                              value="1" <?php echo(($gender ?? '0') === 1 ? 'checked' : '') ?>>女</label>
+                              value="1" <?php echo(($gender ?? '0') === 1 ? 'checked' : '') ?>>Female</label>
             </div>
         </div>
 
         <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">电话号码</label>
+            <label class="uk-form-label" for="form-stacked-text">Phone Number</label>
             <div class="uk-form-controls">
-                <input class="uk-input" name="phone" type="text" placeholder="请输入电话号码"
+                <input class="uk-input" name="phone" type="text" placeholder="Input phone number"
                        value="<?php __($phone ?? ''); ?>">
             </div>
         </div>
-        <button type="submit" class="uk-button uk-button-primary">修改学生</button>
+        <button type="submit" class="uk-button uk-button-primary">Apply</button>
     </form>
 </div>
